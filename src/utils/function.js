15b84ca76c7eb889f9f7f88da5cb961db74fe68d
bastiansmn <bastian.somon@gmail.com>
2021-11-29 00:58:34 +0100

    Deploiement sur la branche

--- src/utils/function.js
+++
@@ -1,7 +0,0 @@
-export default function setActive(anchor) {
-    document.querySelectorAll(".anchor > a")
-        .forEach(e => {
-            e.classList.remove("anchor_active");
-        });
-    document.querySelector(`a[href='#${anchor}']`).classList.add("anchor_active");
-}
